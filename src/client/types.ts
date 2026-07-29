32f214ca19d99a5a2c5c06049f7e334ab79ec7ce
Ian Macartney <366683+ianmacartney@users.noreply.github.com>
2026-07-28 18:25:33 -0700

    drop old type utils

--- src/client/types.ts
+++ src/client/types.ts
@@ -1,12 +1,9 @@
 import type {
-  Expand,
-  FunctionReference,
   GenericActionCtx,
   GenericDataModel,
   GenericMutationCtx,
   GenericQueryCtx,
 } from "convex/server";
-import type { GenericId } from "convex/values";
 
 // Type utils follow
 
@@ -22,34 +19,3 @@ export type ActionCtx = Pick<
   GenericActionCtx<GenericDataModel>,
   "auth" | "runQuery" | "runMutation" | "runAction" | "storage"
 >;
-
-export type OpaqueIds<T> =
-  T extends GenericId<infer _T>
-    ? string
-    : T extends (infer U)[]
-      ? OpaqueIds<U>[]
-      : T extends ArrayBuffer
-        ? ArrayBuffer
-        : T extends object
-          ? {
-              [K in keyof T]: OpaqueIds<T[K]>;
-            }
-          : T;
-
-export type UseApi<API> = Expand<{
-  [mod in keyof API]: API[mod] extends FunctionReference<
-    infer FType,
-    "public",
-    infer FArgs,
-    infer FReturnType,
-    infer FComponentPath
-  >
-    ? FunctionReference<
-        FType,
-        "internal",
-        OpaqueIds<FArgs>,
-        OpaqueIds<FReturnType>,
-        FComponentPath
-      >
-    : UseApi<API[mod]>;
-}>;
